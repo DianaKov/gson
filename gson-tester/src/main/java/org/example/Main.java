8dbcaea25a73fc4ba2111fabfdc1e987732f615d
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-03-10 15:25:22 +0200

    Update Main.java

--- gson-tester/src/main/java/org/example/Main.java
+++ gson-tester/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -9,32 +10,36 @@ import java.net.URL;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String url = "https://api.monobank.ua/bank/currency";
 
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
 
-        con.setRequestMethod("GET");
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                String response = String.join("", in.lines().toArray(String[]::new));
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        Gson gson = new Gson();
-        JsonObject[] rates = gson.fromJson(response.toString(), JsonObject[].class);
-        for (JsonObject rate : rates) {
-            if (rate.get("currencyCodeA").getAsInt() == 840 && rate.get("currencyCodeB").getAsInt() == 980) {
-                double usdRate = rate.get("rateBuy").getAsDouble();
+                double usdRate = getExchangeRate(response);
                 double uahRate = 1 / usdRate;
                 System.out.println("USD rate: " + usdRate);
                 System.out.println("UAH rate: " + uahRate);
-                break;
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static double getExchangeRate(String response) {
+        Gson gson = new Gson();
+        JsonObject[] rates = gson.fromJson(response, JsonObject[].class);
+
+        for (JsonObject rate : rates) {
+            if (rate.get("currencyCodeA").getAsInt() == 840 && rate.get("currencyCodeB").getAsInt() == 980) {
+                return rate.get("rateBuy").getAsDouble();
             }
         }
+        throw new RuntimeException("Exchange rate not found");
     }
 }
